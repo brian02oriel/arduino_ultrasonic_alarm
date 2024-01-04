469be821a270e57dea7a491f4eb9ae286664adfd
Brian Nieto <brian.nieto1@utp.ac.pa>
2024-01-03 22:46:37 -0500

    Adding arduino sketch

---
+++ sketch_ino/sketch_ino.ino
@@ -0,0 +1,39 @@
+#define trigPin 6
+#define echoPin 5
+#define buzzer 2
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop(){
+  long distance = getDistance();
+  activateBuzzer(distance);
+  delay(200);
+}
+
+long getDistance(){
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  return distance;
+}
+
+void activateBuzzer(long distance){
+    float newDelay = (distance * 3) + 30;
+    if(distance < 50) {
+    digitalWrite(buzzer, HIGH);
+    delay(newDelay);
+    digitalWrite(buzzer, LOW);
+  } else {
+    digitalWrite(buzzer,LOW);
+  }  
+}
